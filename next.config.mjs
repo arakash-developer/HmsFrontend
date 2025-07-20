38e100c769c06ac81228fbc13e57ee09a52334e7
akash100a <arakash968786@gmail.com>
2025-07-20 14:29:15 +0600

    fix: remove unused @next/bundle-analyzer dependency from package.json and package-lock.json

--- next.config.mjs
+++ next.config.mjs
@@ -27,37 +27,3 @@ const nextConfig = {
 }
 
 export default nextConfig
-
-// // akash
-// import withBundleAnalyzer from '@next/bundle-analyzer';
-
-// /** @type {import('next').NextConfig} */
-// const baseConfig = {
-//   basePath: process.env.BASEPATH,
-//   redirects: async () => {
-//     return [
-//       {
-//         source: '/',
-//         destination: '/en/dashboards/crm',
-//         permanent: true,
-//         locale: false
-//       },
-//       {
-//         source: '/:lang(en|fr|ar)',
-//         destination: '/:lang/dashboards/crm',
-//         permanent: true,
-//         locale: false
-//       },
-//       {
-//         source: '/((?!(?:en|fr|ar|front-pages|favicon.ico)\\b)):path',
-//         destination: '/en/:path',
-//         permanent: true,
-//         locale: false
-//       }
-//     ];
-//   }
-// };
-
-// export default withBundleAnalyzer({
-//   enabled: process.env.ANALYZE === 'true',
-// })(baseConfig);
