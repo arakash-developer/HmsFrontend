5a8fc3b5880fe08fb21ac77459bfd98f23b39c8d
akash100a <arakash968786@gmail.com>
2025-07-25 20:04:47 +0600

    Add Header and Footer components with responsive design and navigation features

---
+++ src/components/main/Footer.jsx
@@ -0,0 +1,27 @@
+const Footer = () => {
+  return (
+    <>
+      {/* <!-- Footer --> */}
+      <div
+        className="main-content transition-all flex flex-col overflow-hidden mb-10"
+        id="main-content"
+      >
+        <footer className="bg-white dark:bg-[#0c1427] rounded-t-md px-[20px] md:px-[25px] py-[15px] md:py-[20px] text-center">
+          <p>
+            © <span className="text-purple-500">Trezo</span> is Proudly Owned by{" "}
+            <a
+              href="https://envytheme.com/"
+              target="_blank"
+              className="text-primary-500 transition-all hover:underline"
+            >
+              EnvyTheme
+            </a>
+          </p>
+        </footer>
+      </div>
+      {/* <!-- Footer --> */}
+    </>
+  );
+};
+
+export default Footer;
