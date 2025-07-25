1fe8c8dfbc8381b9d3050164c433609f856e0cf9
akash100a <arakash968786@gmail.com>
2025-07-25 17:39:46 +0600

    Refactor DashboardLayout to use className instead of class for JSX elements

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -2112,10 +2112,10 @@ export default function DashboardLayout() {
 
       <Outlet />
          {/* <!-- Footer --> */}
-             <div class="main-content transition-all flex flex-col overflow-hidden mb-10" id="main-content">
-            <footer class="bg-white dark:bg-[#0c1427] rounded-t-md px-[20px] md:px-[25px] py-[15px] md:py-[20px] text-center">
+             <div className="main-content transition-all flex flex-col overflow-hidden mb-10" id="main-content">
+            <footer className="bg-white dark:bg-[#0c1427] rounded-t-md px-[20px] md:px-[25px] py-[15px] md:py-[20px] text-center">
                 <p>
-                    © <span class="text-purple-500">Trezo</span> is Proudly Owned by <a href="https://envytheme.com/" target="_blank" class="text-primary-500 transition-all hover:underline">EnvyTheme</a>
+                    © <span className="text-purple-500">Trezo</span> is Proudly Owned by <a href="https://envytheme.com/" target="_blank" className="text-primary-500 transition-all hover:underline">EnvyTheme</a>
                 </p>
             </footer>
             </div>
