f80d700428ed37727ea24fb6ff5c2378efcadbf2
akash100a <arakash968786@gmail.com>
2025-07-26 13:05:35 +0600

    update image

--- src/components/auth/Register.jsx
+++ src/components/auth/Register.jsx
@@ -8,11 +8,11 @@ const Register = () => {
             <div className="mx-auto px-[12.5px] md:max-w-[720px] lg:max-w-[960px] xl:max-w-[1255px]">
                 <div className="grid grid-cols-1 lg:grid-cols-2 gap-[25px] items-center">
                     <div className="xl:ltr:-mr-[25px] xl:rtl:-ml-[25px] 2xl:ltr:-mr-[45px] 2xl:rtl:-ml-[45px] rounded-[25px] order-2 lg:order-1">
-                        <img src="assets/images/sign-up.jpg" alt="sign-up-image" className="rounded-[25px]" />
+                        <img src="/images/sign-up.jpg" alt="sign-up-image" className="rounded-[25px]" />
                     </div>
                     <div className="xl:ltr:pl-[90px] xl:rtl:pr-[90px] 2xl:ltr:pl-[120px] 2xl:rtl:pr-[120px] order-1 lg:order-2">
-                        <img src="assets/images/logo-big.svg" alt="logo" className="inline-block dark:hidden" />
-                        <img src="assets/images/white-logo-big.svg" alt="logo" className="hidden dark:inline-block" />
+                        <img src="/images/logo-big.svg" alt="logo" className="inline-block dark:hidden" />
+                        <img src="/images/white-logo-big.svg" alt="logo" className="hidden dark:inline-block" />
                         <div className="my-[17px] md:my-[25px]">
                             <h1 className="font-semibold text-[22px] md:text-xl lg:text-2xl mb-[5px] md:mb-[7px]">
                                 Sign Up to Trezo Dashboard
@@ -24,17 +24,17 @@ const Register = () => {
                         <div className="flex items-center justify-between mb-[20px] md:mb-[23px] gap-[12px]">
                             <div className="grow">
                                 <button type="button" className="block text-center w-full rounded-md transition-all py-[8px] md:py-[10.5px] px-[15px] md:px-[25px] text-black dark:text-white border border-[#D6DAE1] bg-white dark:bg-[#0a0e19] dark:border-[#172036] shadow-sm hover:border-primary-500">
-                                    <img src="assets/images/icons/google.svg" className="inline-block" alt="google" />
+                                    <img src="/images/icons/google.svg" className="inline-block" alt="google" />
                                 </button>
                             </div>
                             <div className="grow">
                                 <button type="button" className="block text-center w-full rounded-md transition-all py-[8px] md:py-[10.5px] px-[15px] md:px-[25px] text-black dark:text-white border border-[#D6DAE1] bg-white dark:bg-[#0a0e19] dark:border-[#172036] shadow-sm hover:border-primary-500">
-                                    <img src="assets/images/icons/facebook2.svg" className="inline-block" alt="google" />
+                                    <img src="/images/icons/facebook2.svg" className="inline-block" alt="google" />
                                 </button>
                             </div>
                             <div className="grow">
                                 <button type="button" className="block text-center w-full rounded-md transition-all py-[8px] md:py-[10.5px] px-[15px] md:px-[25px] text-black dark:text-white border border-[#D6DAE1] bg-white dark:bg-[#0a0e19] dark:border-[#172036] shadow-sm hover:border-primary-500">
-                                    <img src="assets/images/icons/apple.svg" className="inline-block" alt="google" />
+                                    <img src="/images/icons/apple.svg" className="inline-block" alt="google" />
                                 </button>
                             </div>
                         </div>
