f185809a82369439f68e477eca7a2f3e09199b87
akash100a <arakash968786@gmail.com>
2025-07-28 19:37:02 +0600

    update react query

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { router } from "@/routes/Router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { RouterProvider } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
+const queryClient = new QueryClient();
+
 function App() {
   return (
     <>
       <AuthProvider>
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+          <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
       </AuthProvider>
     </>
   );
