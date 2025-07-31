edc59b32417a57dce9ea15ca8e8b473d63573c84
akash100a <arakash968786@gmail.com>
2025-07-31 21:53:13 +0600

    update

--- src/components/main/admin/Footer.jsx
+++ src/components/main/admin/Footer.jsx
@@ -5,13 +5,13 @@ const Footer = () => {
 
         <footer className="bg-white dark:bg-[#0c1427] rounded-t-md px-[20px] md:px-[25px] py-[15px] md:py-[20px] text-center">
           <p>
-            © <span className="text-purple-500">Trezo</span> is Proudly Owned by{" "}
+            © <span className="text-purple-500">Hms</span> is Proudly Owned by{" "}
             <a
-              href="https://envytheme.com/"
+            
               target="_blank"
               className="text-primary-500 transition-all hover:underline"
             >
-              EnvyTheme
+              Hms
             </a>
           </p>
         </footer>
