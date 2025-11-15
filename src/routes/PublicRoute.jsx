9f358026d621154264c255c143600496b0dd4db7
akash100a <arakash968786@gmail.com>
2025-11-15 15:09:56 +0600

    Refactor PublicRoute to improve role-based navigation logic

--- src/routes/PublicRoute.jsx
+++ src/routes/PublicRoute.jsx
@@ -1,14 +1,16 @@
 // routes/PublicRoute.jsx
-import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "@contexts/AuthContext";
+import { Navigate, Outlet } from "react-router-dom";
 
 const PublicRoute = () => {
   const { temptoken, loading } = useAuth();
+  // console.log(temptoken.token);
 
   if (loading) return <div>Loading...</div>;
 
-  if (temptoken) return <Navigate to="/admin" />;
-  // if (temptoken?.role === "superadmin") return <Navigate to="/superadmin" />;
+  if (temptoken?.token?.role == "admin") return <Navigate to="/admin" />;
+  if (temptoken?.token?.role == "receiption")
+    return <Navigate to="/receiption" />;
 
   return <Outlet />;
 };
