9fccaf758328705314a14ff56fb4111ccdea3418
akash100a <arakash968786@gmail.com>
2025-11-17 11:43:40 +0600

    Refactor code structure for improved readability and maintainability

--- src/components/receiption/PatientReg.jsx
+++ src/components/receiption/PatientReg.jsx
@@ -282,6 +282,65 @@ const PatientReg = () => {
               </tbody>
             </table>
           </div>
+          <div class="mt-5 px-[20px] py-[12px] md:py-[14px] rounded-b-md border-l border-r border-t  border-gray-100 dark:border-[#172036] sm:flex sm:items-center justify-between">
+            <p class="mb-0 text-sm">Showing 5 of 36 results</p>
+            <ol class="mt-[10px] sm:mt-0">
+              <li class="inline-block mx-[1px] ltr:first:ml-0 ltr:last:mr-0 rtl:first:mr-0 rtl:last:ml-0">
+                <a
+                  href="javascript:void(0);"
+                  class="w-[31px] h-[31px] block leading-[29px] relative text-center rounded-md border border-gray-100 dark:border-[#172036] transition-all hover:bg-primary-500 hover:text-white hover:border-primary-500"
+                >
+                  <span class="opacity-0">0</span>
+                  <i class="material-symbols-outlined left-0 right-0 absolute top-1/2 -translate-y-1/2">
+                    chevron_left
+                  </i>
+                </a>
+              </li>
+              <li class="inline-block mx-[1px] ltr:first:ml-0 ltr:last:mr-0 rtl:first:mr-0 rtl:last:ml-0">
+                <a
+                  href="javascript:void(0);"
+                  class="w-[31px] h-[31px] block leading-[29px] relative text-center rounded-md border border-primary-500 bg-primary-500 text-white"
+                >
+                  1
+                </a>
+              </li>
+              <li class="inline-block mx-[1px] ltr:first:ml-0 ltr:last:mr-0 rtl:first:mr-0 rtl:last:ml-0">
+                <a
+                  href="javascript:void(0);"
+                  class="w-[31px] h-[31px] block leading-[29px] relative text-center rounded-md border border-gray-100 dark:border-[#172036] transition-all hover:bg-primary-500 hover:text-white hover:border-primary-500"
+                >
+                  2
+                </a>
+              </li>
+              <li class="inline-block mx-[1px] ltr:first:ml-0 ltr:last:mr-0 rtl:first:mr-0 rtl:last:ml-0">
+                <a
+                  href="javascript:void(0);"
+                  class="w-[31px] h-[31px] block leading-[29px] relative text-center rounded-md border border-gray-100 dark:border-[#172036] transition-all hover:bg-primary-500 hover:text-white hover:border-primary-500"
+                >
+                  3
+                </a>
+              </li>
+              <li class="inline-block mx-[1px] ltr:first:ml-0 ltr:last:mr-0 rtl:first:mr-0 rtl:last:ml-0">
+                <a
+                  href="javascript:void(0);"
+                  class="w-[31px] h-[31px] block leading-[29px] relative text-center rounded-md border border-gray-100 dark:border-[#172036] transition-all hover:bg-primary-500 hover:text-white hover:border-primary-500"
+                >
+                  4
+                </a>
+              </li>
+              <li class="inline-block mx-[1px] ltr:first:ml-0 ltr:last:mr-0 rtl:first:mr-0 rtl:last:ml-0">
+                <a
+                  href="javascript:void(0);"
+                  class="w-[31px] h-[31px] block leading-[29px] relative text-center rounded-md border border-gray-100 dark:border-[#172036] transition-all hover:bg-primary-500 hover:text-white hover:border-primary-500"
+                >
+                  <span class="opacity-0">0</span>
+                  <i class="material-symbols-outlined left-0 right-0 absolute top-1/2 -translate-y-1/2">
+                    chevron_right
+                  </i>
+                </a>
+              </li>
+            </ol>
+          </div>
         </div>
       </div>
 
